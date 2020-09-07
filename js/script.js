43a03b60281ef6adfcd7e374eda48fbe14616a0b
Ivan <bessob10@gmail.com>
2020-09-07 18:10:10 +0300

    Английский

--- js/script.js
+++ js/script.js
@@ -221,28 +221,18 @@ window.addEventListener("DOMContentLoaded", function () {
   }
 
   // загрузка и отрисовка карточек меню с сервера
-  getResource("http://localhost:3000/menu").then((data) => createCard(data));
-
-  // альтернативная отрисовка карточек без использования класса
-  function createCard(data) {
+  getResource("http://localhost:3000/menu").then((data) => {
     data.forEach(({ img, altimg, title, descr, price }) => {
-      const element = document.createElement("div");
-
-      element.classList.add("menu__item");
-
-      element.innerHTML = `
-                <img src=${img} alt=${altimg}>
-                <h3 class="menu__item-subtitle">${title}</h3>
-                <div class="menu__item-descr">${descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${price}</span> грн/день</div>
-                </div>
-            `;
-      document.querySelector(".menu .container").append(element);
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
     });
-  }
+  });
 
   //========================== FORMS
 
