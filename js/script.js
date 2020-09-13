686fa15b030f3a8d5e57f15b57d279349244cd1a
Ivan <bessob10@gmail.com>
2020-09-13 14:10:52 +0300

    adding calculator of calories

--- js/script.js
+++ js/script.js
@@ -548,4 +548,103 @@ window.addEventListener("DOMContentLoaded", function () {
       selecDot();
     });
   });
+
+  //========================= CALCULATOR
+
+  // элемент с вычисленными данными
+  const result = document.querySelector(".calculating__result span");
+
+  // значения по умолчанию для кнопок, выбранных по умолчанию
+  let sex = "female";
+  let ratio = 1.375;
+  let height;
+  let weight;
+  let age;
+
+  // функция, вычисляющая результат на данных формы (статических и динамических)
+  function calcTotal() {
+    // проверка, что есть все данные
+    if (!sex || !height || !weight || !age || !ratio) {
+      result.textContent = "____";
+      return;
+    }
+    // вычисление данных (формулы для М и Ж отличаются)
+    // и запись в результирующее поле
+    if (sex === "female") {
+      result.textContent = Math.round(
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+      );
+    } else {
+      result.textContent = Math.round(
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+      );
+    }
+  }
+  // вызов функции расчета, чтобы изменить то, что в верстке на '_____'
+  calcTotal();
+
+  // функция для получения статических с нажатых кнопок (информация в них уже забита)
+  function getStaticInformation(parentSelector, activeClass) {
+    // получаем все кнопки на основе родителя
+    const elements = document.querySelectorAll(`${parentSelector} div`);
+
+    // обработчик: если есть атрибут 'data-ratio' (есть только у физ активности),
+    // значит, это элементы из этой группы;
+    // если нет атрибута, значит это группа выбора пола
+    elements.forEach((elem) => {
+      elem.addEventListener("click", (e) => {
+        if (e.target.getAttribute("data-ratio")) {
+          ratio = +e.target.getAttribute("data-ratio");
+        } else {
+          sex = e.target.getAttribute("id");
+        }
+
+        // добавляю класс активности (подсветка нажатой кнопки)
+        elements.forEach((elem) => {
+          elem.classList.remove(activeClass);
+        });
+        e.target.classList.add(activeClass);
+
+        // вызов расчетной функции для автообновления результата при смене кнопки
+        calcTotal();
+      });
+    });
+  }
+
+  // для дефолтной подсветки
+  getStaticInformation("#gender", "calculating__choose-item_active");
+  getStaticInformation(
+    ".calculating__choose_big",
+    "calculating__choose-item_active"
+  );
+
+  // функция для получения динамических данных из полей формы
+  function getDynamicInformation(selector) {
+    // элемент инпута
+    const input = document.querySelector(selector);
+
+    // обработчик выбранного инпута на основе id
+    input.addEventListener("input", () => {
+      switch (input.getAttribute("id")) {
+        // присваиваем переменным для расчета значение из поля ввода
+        case "height":
+          height = +input.value;
+          break;
+        case "weight":
+          weight = +input.value;
+          break;
+        case "age":
+          age = +input.value;
+          break;
+      }
+
+      // пересчет данных после ввода
+      calcTotal();
+    });
+  }
+
+  // запуск работы функций
+  getDynamicInformation("#height");
+  getDynamicInformation("#weight");
+  getDynamicInformation("#age");
 });
